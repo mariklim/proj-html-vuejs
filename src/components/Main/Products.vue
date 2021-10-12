8991a8ed596558590859e28b1ecf69d33e86db5e
mariklim <maria.klimenko889@gmail.com>
2021-10-12 14:08:58 +0200

    aggiunto position absolute per bottoni

--- src/components/Main/Products.vue
+++ src/components/Main/Products.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <div class="container products">
+    <div class="products">
       <div class="our_products">
         <div class="text">
           <h5 class="section_small_title">OUR PRODUCTS</h5>
@@ -14,8 +14,10 @@
       <div class="prod_gallary">
         <ul>
           <li>
-              <button class="btn_prev_next btn_prev"><i class="fas fa-chevron-left"></i></button>
-            <a href="#">
+            <button class="btn_prev_next btn_prev">
+              <i class="fas fa-chevron-left"></i>
+            </button>
+            <a class="margin_left_10" href="#">
               <img
                 src="../../assets/image/choco-chip-cookies-400x510.jpg"
                 alt="choco chip cookies"
@@ -27,7 +29,9 @@
                 alt="strawberry jam cookies"
               />
             </a>
-             <button class="btn_prev_next btn_next"><i class="fas fa-chevron-right"></i></button>
+            <button class="btn_prev_next btn_next">
+              <i class="fas fa-chevron-right"></i>
+            </button>
           </li>
         </ul>
       </div>
@@ -44,38 +48,43 @@ export default {
 <style lang="scss">
 @import "../../assets/style/common";
 @import "../../assets/style/variables";
-.container {
-  &.products {
-    @include container_80;
-    display: flex;
-    margin-top: 3.125rem;
-    margin-bottom: 3.125rem;
-    .our_products {
-      width: 30%;
-      & .text {
-        padding-right: 1.25rem;
+.products {
+  @include container_80;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 3.125rem;
+  margin-bottom: 3.125rem;
+  background-color: cadetblue;
+  .our_products {
+    width: 30%;
+    & .text {
+      padding-right: 1.25rem;
+    }
+  }
+  .prod_gallary {
+    li {
+      display: flex;
+      align-items: center;
+      position: relative;
+    }
+    li a {
+      max-width: 25rem;
+      img {
+        width: 100%;
       }
     }
-    .prod_gallary {
-      li {
-        display: flex;
-        align-items: center;
-      }
-      li a {
-        max-width: 25rem;
-        img {
-          width: 100%;
-        }
-      }
-      button.btn_next{
-          position: relative;
-          right:1.875rem;
-      }
-      button.btn_prev{
-          position: relative;
-          left:1.875rem;
-      }
+    button.btn_next {
+      position: absolute;
+      left: calc(100% - 1.875rem);
     }
+    button.btn_prev {
+      position: absolute;
+      right: calc(100% - 1.875rem);
+    }
+  }
+  //margin destro per spaziare immagini per first child per tag a
+  .margin_left_10 {
+    margin-right: 0.625rem;
   }
 }
 </style>
