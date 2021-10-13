2439a66cb62ce4e7c2cd1b2fa024d0b14cd5c0b9
mariklim <maria.klimenko889@gmail.com>
2021-10-13 18:37:22 +0200

    aggiunto adress

--- src/components/Main/Contacts.vue
+++ src/components/Main/Contacts.vue
@@ -5,23 +5,44 @@
       <h3>Visit our Bakeries</h3>
     </div>
     <div class="container_contacts">
-        <div class="contact">
-            <div class="contact_img">
-                <img src="../../assets/image/new-york-bk-600x397.jpg" alt="bakery">
-            </div>
-            <div class="adress_1">
-                ciao
-            </div>
+      <div class="contact">
+        <div class="contact_img">
+          <img src="../../assets/image/new-york-bk-600x397.jpg" alt="bakery" />
         </div>
-          <div class="contact">
-            <div class="contact_img">
-                <img src="../../assets/image/london-bk-600x397.jpg" alt="bakery">
+        <div class="adress NY">
+          <h3>New York</h3>
+          <div class="adress_data">
+            <div class="phon_number">
+              <h5>CALL US</h5>
+              <p>1.800.458.556</p>
             </div>
-            <div class="adress_2">
-                ciao
+            <div class="hours_open">
+              <h5>OPEN ALL WEEK</h5>
+              <p>9:00 AM – 6:00 PM</p>
             </div>
+            <button class="btn_white">View Map</button>
+          </div>
         </div>
-        
+      </div>
+      <div class="contact">
+        <div class="contact_img">
+          <img src="../../assets/image/london-bk-600x397.jpg" alt="bakery" />
+        </div>
+        <div class="adress LONDON">
+          <h3>london</h3>
+          <div class="adress_data">
+             <div class="phon_number">
+              <h5>CALL US</h5>
+              <p>1.800.458.556</p>
+            </div>
+            <div class="hours_open">
+              <h5>OPEN ALL WEEK</h5>
+              <p>9:00 AM – 6:00 PM</p>
+            </div>
+            <button class="btn_white">View Map</button>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -42,18 +63,28 @@ section {
   .text {
     text-align: center;
   }
-  .container_contacts{
+  .container_contacts {
     display: flex;
-    .adress_1{
-      height: 1.875rem;
-      background-color: $sky;
-    }
-    .adress_2{
-      height: 1.875rem;
-      background-color: $light_pink;
-    }
+    .adress {
+      text-align: center;
+      &.NY {
+        background-color: $sky;
+      }
+      &.LONDON {
+        background-color: $light_pink;
+      }
+      .adress_data {
+    
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        padding-bottom: 1.875rem;
 
+         p{
+           padding: .1875rem 0
+         }
+      }
     }
-  
+  }
 }
 </style>
