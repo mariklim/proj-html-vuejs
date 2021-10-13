2ef6314256fa9d0b1d0564d0a1b1e396003c83fe
mariklim <maria.klimenko889@gmail.com>
2021-10-13 19:23:16 +0200

    aggiunto base per section social

--- src/components/Main/Contacts.vue
+++ src/components/Main/Contacts.vue
@@ -1,48 +1,64 @@
 <template>
   <section>
-    <div class="text">
-      <h5>LOCATIONS</h5>
-      <h3>Visit our Bakeries</h3>
-    </div>
-    <div class="container_contacts">
-      <div class="contact">
-        <div class="contact_img">
-          <img src="../../assets/image/new-york-bk-600x397.jpg" alt="bakery" />
+    <section class="visit_shop">
+      <div class="text">
+        <h5>LOCATIONS</h5>
+        <h3>Visit our Bakeries</h3>
+      </div>
+      <div class="container_contacts">
+        <div class="contact">
+          <div class="contact_img">
+            <img
+              src="../../assets/image/new-york-bk-600x397.jpg"
+              alt="bakery"
+            />
+          </div>
+          <div class="adress NY">
+            <h3>New York</h3>
+            <div class="adress_data">
+              <div class="phon_number">
+                <h5>CALL US</h5>
+                <p>1.800.458.556</p>
+              </div>
+              <div class="hours_open">
+                <h5>OPEN ALL WEEK</h5>
+                <p>9:00 AM – 6:00 PM</p>
+              </div>
+              <button class="btn_white">View Map</button>
+            </div>
+          </div>
         </div>
-        <div class="adress NY">
-          <h3>New York</h3>
-          <div class="adress_data">
-            <div class="phon_number">
-              <h5>CALL US</h5>
-              <p>1.800.458.556</p>
+        <div class="contact">
+          <div class="contact_img">
+            <img src="../../assets/image/london-bk-600x397.jpg" alt="bakery" />
+          </div>
+          <div class="adress LONDON">
+            <h3>london</h3>
+            <div class="adress_data">
+              <div class="phon_number">
+                <h5>CALL US</h5>
+                <p>1.800.458.556</p>
+              </div>
+              <div class="hours_open">
+                <h5>OPEN ALL WEEK</h5>
+                <p>9:00 AM – 6:00 PM</p>
+              </div>
+              <button class="btn_white">View Map</button>
             </div>
-            <div class="hours_open">
-              <h5>OPEN ALL WEEK</h5>
-              <p>9:00 AM – 6:00 PM</p>
-            </div>
-            <button class="btn_white">View Map</button>
           </div>
         </div>
       </div>
-      <div class="contact">
-        <div class="contact_img">
-          <img src="../../assets/image/london-bk-600x397.jpg" alt="bakery" />
-        </div>
-        <div class="adress LONDON">
-          <h3>london</h3>
-          <div class="adress_data">
-             <div class="phon_number">
-              <h5>CALL US</h5>
-              <p>1.800.458.556</p>
-            </div>
-            <div class="hours_open">
-              <h5>OPEN ALL WEEK</h5>
-              <p>9:00 AM – 6:00 PM</p>
-            </div>
-            <button class="btn_white">View Map</button>
-          </div>
-        </div>
-      </div>
+    </section>
+    <section class="social">
+        <div class="square_img">a</div>
+        <div class="box_CTA">a</div>
+        <div class="square_img">a</div>
+    </section>
+    <div class="social">
+      <div class="square_img">a</div>
+      <div class="square_img">a</div>
+      <div class="square_img">a</div>
+      <div class="square_img">a</div>
     </div>
   </section>
 </template>
@@ -56,14 +72,16 @@ export default {
 <style lang="scss" scoped>
 @import "../../assets/style/common";
 @import "../../assets/style/variables";
-section {
+.visit_shop {
   @include margin_top_bottom_70px;
   @include container_80;
   @include section_header;
+  margin-bottom: 4.375rem;
   .text {
     text-align: center;
   }
   .container_contacts {
+    justify-content: space-between;
     display: flex;
     .adress {
       text-align: center;
@@ -74,17 +92,43 @@ section {
         background-color: $light_pink;
       }
       .adress_data {
-    
         display: flex;
         justify-content: space-around;
         align-items: center;
         padding-bottom: 1.875rem;
 
-         p{
-           padding: .1875rem 0
-         }
+        p {
+          padding: 0.1875rem 0;
+        }
       }
     }
   }
+}
+.social {
+  width: 100%;
+  height: 250px;
+  display: flex;
+  // @include margin_top_bottom_70px;
+  @include container_80;
+  @include section_header;
+  margin-bottom: 10px;
+ 
+    .square_img {
+      margin: 0 10px;
+      width: calc(25% - 20px);
+      height: 100%;
+      background-color: violet;
+      
+   
+    }
+    .box_CTA {
+      margin: 0 10px;
+      width: calc(50% - 20px);
+      height: 100%;
+      background-color: $main_color_light;
+      
+ 
+    }
+
 }
 </style>
