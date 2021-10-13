5faf6a50db358c485f9e41cc6672357646d954c9
mariklim <maria.klimenko889@gmail.com>
2021-10-13 21:11:42 +0200

    aggiunto images

--- src/components/Main/Contacts.vue
+++ src/components/Main/Contacts.vue
@@ -50,15 +50,26 @@
       </div>
     </section>
     <section class="social">
-        <div class="square_img">a</div>
-        <div class="box_CTA">a</div>
-        <div class="square_img">a</div>
+        <div class="square_img">
+          <img src="../../assets/image/social-1.jpg" alt="social">
+        </div>
+        <div class="box_CTA">
+          <div class="text">
+            <h5>MADE FOR SHARING</h5>
+            <h3>Let's stay in touch</h3>
+            <button class="btn_lilac"> <span><i class="fab fa-instagram"></i></span>
+              Follow us</button>
+          </div>
+        </div>
+        <div class="square_img">
+          <img src="../../assets/image/social-2.jpg" alt="social">
+        </div>
     </section>
     <div class="social">
-      <div class="square_img">a</div>
-      <div class="square_img">a</div>
-      <div class="square_img">a</div>
-      <div class="square_img">a</div>
+      <div class="square_img"><img src="../../assets/image/social-3.jpg" alt="social"></div>
+      <div class="square_img"><img src="../../assets/image/social-4.jpg" alt="social"></div>
+      <div class="square_img"><img src="../../assets/image/social-5.jpg" alt="social"></div>
+      <div class="square_img"><img src="../../assets/image/social-6.jpg" alt="social"></div>
     </div>
   </section>
 </template>
@@ -83,6 +94,7 @@ export default {
   .container_contacts {
     justify-content: space-between;
     display: flex;
+  
     .adress {
       text-align: center;
       &.NY {
@@ -108,7 +120,7 @@ export default {
   width: 100%;
   height: 250px;
   display: flex;
-  // @include margin_top_bottom_70px;
+  @include margin_top_bottom_70px;
   @include container_80;
   @include section_header;
   margin-bottom: 10px;
@@ -117,15 +129,30 @@ export default {
       margin: 0 10px;
       width: calc(25% - 20px);
       height: 100%;
-      background-color: violet;
+      overflow: hidden;
+        img{
+          width: 100%;
+          object-fit: cover;
+        }
+     
+      
       
    
     }
     .box_CTA {
+      display: flex;
+      justify-content: center;
+      align-items: center;
       margin: 0 10px;
       width: calc(50% - 20px);
       height: 100%;
       background-color: $main_color_light;
+        .text{
+          text-align: center;
+        }
+        button span{
+          margin-right: .25rem;
+        }
       
  
     }
