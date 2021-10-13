67ca048dc1fcbe42d8dd055b38be31bfb654d8c0
mariklim <maria.klimenko889@gmail.com>
2021-10-13 18:18:21 +0200

    aggiunto section contatti

---
+++ src/components/Date/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <div class="card">
+      <h3>{{ infoImage.name }}</h3>
+      <img :src="infoImage.src" :alt="infoImage.name" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: ['infoImage'],
+};
+</script>
+
+<style>
+</style>
--- src/components/Main/Contacts.vue
+++ src/components/Main/Contacts.vue
@@ -1,15 +1,59 @@
 <template>
-<div>Visit shop, Social</div>
-  
+  <section>
+    <div class="text">
+      <h5>LOCATIONS</h5>
+      <h3>Visit our Bakeries</h3>
+    </div>
+    <div class="container_contacts">
+        <div class="contact">
+            <div class="contact_img">
+                <img src="../../assets/image/new-york-bk-600x397.jpg" alt="bakery">
+            </div>
+            <div class="adress_1">
+                ciao
+            </div>
+        </div>
+          <div class="contact">
+            <div class="contact_img">
+                <img src="../../assets/image/london-bk-600x397.jpg" alt="bakery">
+            </div>
+            <div class="adress_2">
+                ciao
+            </div>
+        </div>
+        
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-    name:"Contacts"
-
-}
+  name: "Contacts",
+};
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+@import "../../assets/style/common";
+@import "../../assets/style/variables";
+section {
+  @include margin_top_bottom_70px;
+  @include container_80;
+  @include section_header;
+  .text {
+    text-align: center;
+  }
+  .container_contacts{
+    display: flex;
+    .adress_1{
+      height: 1.875rem;
+      background-color: $sky;
+    }
+    .adress_2{
+      height: 1.875rem;
+      background-color: $light_pink;
+    }
 
+    }
+  
+}
 </style>
