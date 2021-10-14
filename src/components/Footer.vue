0aa0311ce5f15521cbe419d69df3660cf7b1e934
mariklim <maria.klimenko889@gmail.com>
2021-10-14 09:44:49 +0200

    aggiunto footer-top

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,48 @@
 <template>
-  <section class="footer-hero">
-    <div class="container hero">
-      <div class="text">
-        <h5>TRY OUR SEASONAL PRODUCTS</h5>
-        <h3>Order for pick-up or delivery to your home</h3>
+  <footer>
+    <section class="footer-hero">
+      <div class="container hero">
+        <div class="text">
+          <h5>TRY OUR SEASONAL PRODUCTS</h5>
+          <h3>Order for pick-up or delivery to your home</h3>
+        </div>
+        <button class="btn_white">Shop Now</button>
       </div>
-      <button class="btn_white">Shop Now</button>
-    </div>
-  </section>
+    </section>
+    <section class="footer-top">
+      <div class="logo">
+        <img src="../assets/image/avada-bakery-logo.png" alt="logo" />
+      </div>
+      <div>
+        <h3>Subscribe to our Newsletter</h3>
+        <input type="text" placeholder="Enter email address*" />
+        <button class="btn_lilac">Subscribe</button>
+      </div>
+    </section>
+    <section class="footer-nav">
+      <ul>
+        <li>
+          <Link v-for="(link, index) in links" :key="index" :link="link" />
+          <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </li>
+      </ul>
+    </section>
+  </footer>
 </template>
 
 <script>
+import elm from "./Date/links.json";
+import Link from "./Date/Link.vue";
 export default {
-  name: "Hero",
+  name: "Footer",
+  components: {
+    Link,
+  },
+  data() {
+    return {
+      links: elm,
+    };
+  },
 };
 </script>
 
@@ -26,7 +56,7 @@ export default {
   background-image: url("../assets/image/call-to-action-bg.jpg");
   background-size: cover;
   padding: 5rem 0;
-  color: white; 
+  color: white;
   .container {
     &.hero {
       padding-left: 5rem;
@@ -41,5 +71,26 @@ export default {
       }
     }
   }
+}
+.footer-top {
+  @include margin_top_bottom_70px;
+  @include container_80;
+  @include section_header;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: right;
+  h3 {
+    font-family: "Work Sans", sans-serif;
+    font-size: 1.125rem;
+  }
+  input {
+    padding: 0.4375rem 1.25rem;
+    margin-right: 0.3125rem;
+  }
+}
+.footer-nav {
+  @include container_80;
+  margin-top: 1.25rem;
 }
 </style>
